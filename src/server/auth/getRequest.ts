0b7adf66e12d5f9781b0b0531b5cd2755e201fa2
DeathNeroTV <runge.roman@gmail.com>
2021-06-10 17:45:18 +0200

    update .env data

--- src/server/auth/getRequest.ts
+++ src/server/auth/getRequest.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const azureURL = process.env.ENDPOINT ? process.env.ENDPOINT : `https://deathnerotv.github.io/triallife/`;
 const azureRedirect = encodeURI(`${azureURL}/v1/request/key`);
 const url = `https://discord.com/api/oauth2/authorize?client_id=660200376523292725&redirect_uri=${azureRedirect}&prompt=none&response_type=code&scope=identify`;
