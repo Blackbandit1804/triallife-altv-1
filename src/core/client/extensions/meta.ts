052cb983141f1cde61397fbd72253985c1885a6e
DeathNeroTV <runge.roman@gmail.com>
2021-06-16 13:03:48 +0200

    patched some errors

--- src/core/client/extensions/meta.ts
+++ src/core/client/extensions/meta.ts
@@ -4,7 +4,7 @@ import { Vehicle } from '../../shared/interfaces/vehicle';
 
 export interface Meta {
     permissionLevel: number; // Used to determine the player's current permissionLevel as a player..
-    isDead: boolean; // Used to determine when a player is dead or not.
+    isUnconscious: boolean; // Used to determine when a player is dead or not.
     gridSpace: number; // Used to identify what part of the map the player is on. Based on Y Axis.
 
     // Currency
