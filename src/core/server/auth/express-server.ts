8e5e5e1fdf411d4a1c2ddc4e4d2320a2bf14940c
DeathNeroTV <runge.roman@gmail.com>
2021-06-17 00:40:16 +0200

    remove useless async states

--- src/core/server/auth/express-server.ts
+++ src/core/server/auth/express-server.ts
@@ -1,16 +1,11 @@
-import * as alt from 'alt-server';
-import axios from 'axios';
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import Logger from '../utility/tlrp-logger';
-import { LoginManager } from '../systems/login';
 import { getPublicKey } from '../utility/encryption';
 
 const app = express();
 app.use(cors());
 
-app.get('/v1/get/version', (req, res) => res.send('1.0.0'));
 app.get('/v1/get/key', async (req, res) => res.send(JSON.stringify({ status: true, key: getPublicKey() })));
 
 app.listen(7800, () => Logger.info('Authentication service listen on port 7800'));
